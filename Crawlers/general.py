1fc1201d6e030f3a81fb6a6488b542e6219d6158
John Lloyd Divino <lloyd@codeninja.co>
2018-03-30 16:36:17 +0800

    Added queue and crawled methods

--- Crawlers/general.py
+++ Crawlers/general.py
@@ -2,8 +2,27 @@ import os
 
 #Each folder means a certain website
 def create_project_dir(directory):
-    if not os.path.exists(directory):
-        print('Creating a new project for ' + directory)
-        os.makedirs(directory)
+    dir = './Websites' + directory
+    if not os.path.exists(dir):
+        print('Creating a new project for ' + dir)
+        os.makedirs(dir)
 
-create_project_dir("./Websites/9gag")
+
+# Create queue and craled for each website
+def create_queue_crawled(project_name, base_url):
+    dir = './Websites/'
+    queue = dir + project_name + '/queue.txt'
+    crawled = dir + project_name + '/crawled.txt'
+    if not os.path.isfile(queue):
+        print('Creating queue file ', queue)
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        print('Creating crawled file ', crawled)
+        write_file(crawled, '')
+
+# writes data into a txt format
+def write_file(path, data):
+    with open(path, 'w') as data_file:
+        data_file.write(data)
+
+create_queue_crawled('9gag', 'https://9gag.com/')
