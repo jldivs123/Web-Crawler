4d51996bec54e9f9295ad6274900644a00aee5b4
John Lloyd Divino <lloyd@codeninja.co>
2018-04-09 18:58:45 +0800

    Added a spider bot

--- Crawlers/link_finder.py
+++ Crawlers/link_finder.py
@@ -5,7 +5,7 @@ class LinkFinder(HTMLParser):
     def __init__(self, base_url, page_url):
         super().__init__()
         self.base_url = base_url
-        self.page_url = url
+        self.page_url = page_url
         self.links = set()
 
     def handle_starttag(self, tag, attrs):
